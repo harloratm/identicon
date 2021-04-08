3ebfb41c9b060cd251242704b50c44063fb64b4f
Ascani Carlo <carlo.ratm@gmail.com>
2021-04-08 19:15:49 +0200

    Remove config and refactor mix.exs

--- config/config.exs
+++
@@ -1,4 +0,0 @@
-import Config
-
-config :identicon,
-  life: "42"
--- mix.exs
+++ mix.exs
@@ -1,34 +1,21 @@
 defmodule Identicon.MixProject do
   use Mix.Project
 
-  def project do
-    IO.puts(Application.get_env(:identicon, :life, ""))
+  @default_project [
+    app: :identicon,
+    escript: [main_module: Identicon.CLI],
+    version: "0.1.0",
+    elixir: "~> 1.11",
+    start_permanent: false,
+    deps: [{:egd, github: "erlang/egd"}]
+  ]
 
-    Keyword.merge(
-      [
-        app: :identicon,
-        escript: [main_module: Identicon.CLI],
-        version: "0.1.0",
-        elixir: "~> 1.11",
-        start_permanent: Mix.env() == :prod,
-        deps: deps()
-      ],
-      env_conf(Mix.env())
-    )
-  end
+  def project, do: Keyword.merge(@default_project, project(Mix.env()))
+
+  def project(:gui), do: [app: :wxidenticon, escript: [main_module: Identicon.GUI]]
+  def project(_), do: []
 
   def application do
-    [
-      extra_applications: [:logger, :crypto, :wx]
-    ]
+    [extra_applications: [:logger, :crypto, :wx]]
   end
-
-  defp deps do
-    [
-      {:egd, github: "erlang/egd"}
-    ]
-  end
-
-  defp env_conf(:gui), do: [app: :wxidenticon, escript: [main_module: Identicon.GUI]]
-  defp env_conf(_), do: []
 end

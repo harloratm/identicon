840c9623ea096e4fa5b3836fc0dee26a8e6fb5c6
Ascani Carlo <carlo.ratm@gmail.com>
2021-04-13 08:21:36 +0200

    Add basic configuration for web api

--- apps/webapi/lib/webapi/application.ex
+++ apps/webapi/lib/webapi/application.ex
@@ -5,11 +5,14 @@ defmodule Webapi.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {Plug.Cowboy, scheme: :http, plug: Webapi.Router, options: [port: 8080]}
+      {Plug.Cowboy, scheme: cowboy_scheme(), plug: Webapi.Router, options: [port: cowboy_port()]}
     ]
 
     opts = [strategy: :one_for_one, name: Webapi.Supervisor]
-    Logger.info("Starting application...")
+    Logger.info("Starting server at #{cowboy_scheme()}://127.0.0.1:#{cowboy_port()} ...")
     Supervisor.start_link(children, opts)
   end
+
+  defp cowboy_port, do: Application.get_env(:webapi, :cowboy_port, 8080)
+  defp cowboy_scheme, do: Application.get_env(:webapi, :cowboy_scheme, :http)
 end
